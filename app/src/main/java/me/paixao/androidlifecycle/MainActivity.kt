6261c74c61be39da1037475758cc3a694820e21d
pedropaixao <pedro.paixao@osit-group.com>
2018-03-27 16:45:21 +0100

    Added logs for all callbacks of activity and of fragment

--- app/src/main/java/me/paixao/androidlifecycle/MainActivity.kt
+++ app/src/main/java/me/paixao/androidlifecycle/MainActivity.kt
@@ -2,11 +2,48 @@ package me.paixao.androidlifecycle
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private fun printLog(s:String) {
+        // display a message in Logcat
+        Log.e("Activity Lifecycle:", s)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        printLog("onCreate Called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        printLog("onStart Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        printLog("onRestart Called")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        printLog("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        printLog("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        printLog("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        printLog("onDestroy Called")
     }
 }
---
+++ app/src/main/java/me/paixao/androidlifecycle/MainFragment.kt
@@ -0,0 +1,78 @@
+package me.paixao.androidlifecycle
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment: Fragment() {
+
+    private fun printLog(s:String) {
+        // display a message in Logcat
+        Log.e("Fragment Lifecycle:", s)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        printLog("onActivityCreated Called")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_main, container, false)
+        printLog("onCreateView Called")
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        printLog("onViewCreated Called")
+    }
+
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+        printLog("onAttach Called")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        printLog("onCreate Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        printLog("onDestroy Called")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        printLog("onDestroyView Called")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        printLog("onDetach Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        printLog("onPause Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        printLog("onResume Called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        printLog("onStart Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        printLog("onStop Called")
+    }
+}
